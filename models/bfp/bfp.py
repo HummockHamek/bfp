3f54288598c38004c3505c177695044116f72b7d
HummockHamek <ub3555@gmail.com>
2025-03-29 19:44:04 +0530

    aA

--- models/bfp/bfp.py
+++ models/bfp/bfp.py
@@ -7,7 +7,7 @@ import copy
 import math
 import torch
 import wandb
-import CustomLoss
+
 import numpy as np
 import matplotlib.pyplot as plt
 import torch.nn as nn
@@ -24,6 +24,34 @@ from utils.routines import forward_loader_all_layers
 from .args import set_best_args
 from .utils import *
 from .projector_manager import ProjectorManager
+
+class CustomLoss(nn.Module):
+    def __init__(self, lambda1=0.1, lambda2=0.1, lambda3=0.05, feature_dim=128):
+        super(CustomLoss, self).__init__()
+        self.lambda1 = lambda1
+        self.lambda2 = lambda2
+        self.lambda3 = lambda3
+        self.similarity_metric = nn.CosineSimilarity(dim=1)
+        self.feature_dim = feature_dim
+
+    def forward(self, inputs, buf_inputs, feats, buf_feats, norm_inputs, norm_buf_inputs):
+        # Compute similarity loss
+        similarity_loss = torch.mean(1 - self.similarity_metric(feats, buf_feats))
+
+        # Compute norm difference loss
+        norm_loss = torch.abs(norm_inputs - norm_buf_inputs).mean()
+
+        # Compute variance regularization loss
+        variance_loss = torch.var(feats) + torch.var(buf_feats)
+
+        # Total loss
+        total_loss = (
+            self.lambda1 * similarity_loss +
+            self.lambda2 * norm_loss +
+            self.lambda3 * variance_loss
+        )
+        return total_loss
+
 
 class Bfp(ContinualModel):
     NAME = 'bfp'
